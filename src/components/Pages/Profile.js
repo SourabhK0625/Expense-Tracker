75f7a68368464bf7c363ba86e125aea2fd717804
SourabhK0625 <106949506+SourabhK0625@users.noreply.github.com>
2022-07-25 17:10:30 +0530

    Edit user data

--- src/components/Pages/Profile.js
+++ src/components/Pages/Profile.js
@@ -1,10 +1,30 @@
-import React, {useRef} from "react";
+import React, {useRef , useEffect , useState} from "react";
 import { Link } from "react-router-dom";
 import './Profile.css'
 const Profile = props =>
 {
+    const [name , setIsName] = useState([]);
+    const [imageLink , setImageLink] = useState([]);
     const nameRef = useRef();
     const imageUrlRef = useRef();
+
+    async function getBackData()
+    {
+        const response = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyBMoCmrXulQToPQEvD8GVvnW5klI3An3Ps',
+        {
+            method: 'POST',
+            body: JSON.stringify({
+                idToken: localStorage.getItem('token')
+            })
+        }) 
+        const data = await response.json();
+        // const userData = data.users;
+        console.log(data)
+        setIsName(data.displayName);
+        setImageLink(data.photoUrl);
+    } 
+    useEffect(()=>{getBackData()},[]);
+
     const submitHandler = item =>
     {
         item.preventDefault();
@@ -35,12 +55,10 @@ const Profile = props =>
             <div>
                 <form onSubmit={submitHandler}>
                     <label htmlFor='name'>Full Name :</label>
-                    <input type='text' id='name' required ref={nameRef}/>
-
+                    <input type='text' id='name' required ref={nameRef} defaultValue={name} />
                     <label htmlFor='imageUrl'>Profile Photo Url :</label>
-                    <input type='text' id='imageUrl' required ref={imageUrlRef}/>
+                    <input type='text' id='imageUrl' required ref={imageUrlRef} defaultValue={imageLink} />
                     <button type="submit">Update</button>
-
                 </form>
                 
             </div>
