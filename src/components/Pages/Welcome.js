053c0df56253cdf68cdadcd1e6dcc927467054fb
SourabhK0625 <106949506+SourabhK0625@users.noreply.github.com>
2022-07-26 19:31:05 +0530

    Updated

--- src/components/Pages/Welcome.js
+++ src/components/Pages/Welcome.js
@@ -1,4 +1,5 @@
 import React, {useRef, useState , useEffect } from "react";
+import expenses from '../../Assets/expenses.jpg';
 import { Link } from "react-router-dom";
 import './Welcome.css'
 
@@ -68,7 +69,7 @@ const Welcome = props =>
         }).then((data) => {
             console.log(data)
             alert('Data is sent to Backend successfully!!!')
-            setArrayDetails([...arrayDetails, {amount:enteredAmount, desciption: enteredDescription, category: enteredExpense}])
+            setArrayDetails([...arrayDetails, {amount:enteredAmount, description: enteredDescription, category: enteredExpense}])
             
         }).catch(err =>{
             alert(err.errorMessage)
@@ -80,31 +81,33 @@ const Welcome = props =>
                 <span>Welcome To Expense Tracker...!</span>
                 <span className="profile">Complete Your Profile..! <Link to='/profile' className="complete">Complete Now?</Link></span>
             </div>
-            <form onSubmit={submitHandler}>
-                <span>
-                    <label htmlFor='amount'>Amount Spent Rs :</label>
-                    <input type='number' min='0' id='amount' ref={amountRef}/>
-                </span>
-                <span>
-                    <label htmlFor='description'>Description :</label>
-                    <input type='text' id='description' ref={descriptionRef}/>
-                </span>
-                <span>
-                    <label htmlFor='category'>Category :</label>
-                    <select id="Expesnse" name="Expense" ref={expenseRef}>
-                        <option value="Food">Food</option>
-                        <option value="Movie">Movie</option>
-                        <option value="Petrol">Petrol</option>
-                        <option value="Shopping">Shopping</option>
-                        <option value="Personal">Personal</option>
-                        <option value="Other">Other</option>
+            <form className="form" onSubmit={submitHandler}>
+            <img alt="" className="image-expenses" src={expenses}></img>
+            <h1>Enter Details Here</h1>
+                <span className="form-span">
+                    <label className="form-lable" htmlFor='amount'>Amount Spent Rs :</label>
+                    <input className="form-input-AE" type='number' min='0' id='amount' ref={amountRef}/>
+                </span><br></br>
+                <span className="form-span">
+                    <label className="form-lable" htmlFor='description'>Description :</label>
+                    <input className="form-input-description" type='text' id='description' ref={descriptionRef}/>
+                </span><br></br>
+                <span className="form-span">
+                    <label className="form-lable" htmlFor='category'>Category :</label>
+                    <select className="form-input-AE" id="Expesnse" name="Expense" ref={expenseRef}>
+                        <option className="form-option" value="Food">Food</option>
+                        <option className="form-option" value="Movie">Movie</option>
+                        <option className="form-option" value="Petrol">Petrol</option>
+                        <option className="form-option" value="Shopping">Shopping</option>
+                        <option className="form-option" value="Personal">Personal</option>
+                        <option className="form-option" value="Other">Other</option>
                     </select>
-                </span>
+                </span><br></br>
                 <span>
-                    <button type="submit">Submit Expense</button>
-                </span>
+                    <button className="submit-expense" type="submit">Submit Expense</button>
+                </span><br></br>
             </form>
-            <ul>
+            <ul className="expenses">
                 {arrayDetails.map(item=>{return(
                     <li key={Math.random.toString()}>Rs{item.amount}  {item.description}  {item.expenses}</li>
                 )})}
